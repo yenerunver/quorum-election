66c3c31016993c1e917478cfc9a90b2fc5aa9736
Yener Ünver <yenerunver@hotmail.com>
2019-05-22 19:46:43 +0000

    initial commit

--- contracts/TurkishElections.sol
+++ contracts/TurkishElections.sol
@@ -1,11 +1,10 @@
-pragma solidity 0.5.0;
-
-contract TurkishElections {
+pragma solidity 0.5.0; contract TurkishElections {
     // An address array for storing votes
     mapping(address => uint) private votes;
-
     // An integer array for storing total results
     mapping(uint => uint) private totals;
+
+    event votedBy(address voter);
 
     // Definition of 4 voters for demo purpose
     // todo: Get voter information from Turkish Supreme Election Council (SEC) database
@@ -19,7 +18,20 @@ contract TurkishElections {
     // todo: Get election information via API provided by SEC
     uint start = 1561269600; // Sunday, 23 June 2019 09:00:00 GMT+03:00
     uint end = 1561298400; // Sunday, 23 June 2019 17:00:00 GMT+03:00
-
+    
+    // MODIFIERS
+     modifier onlyValidVoter(){
+        address voter = msg.sender;
+        require((voter == voterA || voter == voterB || voter == voterC || voter == voterD || voter == voterE),"Intruder alert!");
+        _;
+    }
+    
+    modifier onlyElectionTime(){
+        // Validate date and time
+        require(now < start, "Election is not started yet!");
+        require(now > end, "Election is over!");
+        _;
+    }
     // todo: Modify the constructor according to the data provided by SEC
     constructor(address _voterA, address _voterB, address _voterC, address _voterD, address _voterE) public {
         voterA = _voterA;
@@ -28,45 +40,39 @@ contract TurkishElections {
         voterD = _voterD;
         voterE = _voterE;
     }
-
     // Definition of vote() function with 6 options for demo purpose
     // todo: Get party/candidate information from SEC database
-    function vote(uint _option) public {
-        // Validate date and time
-        require(now >= start, "Election is not started yet!");
-        require(now <= end, "Election is over!");
-        
-        // Validate voter
-        require(validateVoter(msg.sender), "Intruder alert!");
+    function vote(uint _option) public onlyValidVoter{
         
         // Validate if user has not voted before
         // todo: Validate user vote via API provided by SEC
         require(votes[msg.sender] == 0, "Duplicate voting trial!");
         
-        // Validate options
-        require(validateOption(_option), "Invalid candidate!");
-
         // Save user vote
         votes[msg.sender] = _option;
-
         // Save total votes
         totals[_option] = totals[_option] + 1;
+        emit votedBy(msg.sender);
     }
-
-    // Definition of getTotalVotes() function to show total results
-    function getTotalVotes(uint _option) view public returns (uint total) {
-        return totals[_option];
-    }
-    
     // Definition of validateVoter() function to validate voter
     // todo: Validate voter via APIs provided by SEC and Turkish Central Civil Registration System (MERNIS)
     function validateVoter(address voter) view public returns (bool result){
         return (voter == voterA || voter == voterB || voter == voterC || voter == voterD || voter == voterE);
     }
     
+    // Definition of getTotalVotes() function to show total results
+    function getTotalVotes(uint _option) view public returns (uint total) {
+        return totals[_option];
+    }
+         
     // Definition of validateOption() function to validate option
     // todo: Validate options via API provided by SEC
     function validateOption(uint option) pure public returns (bool result){
         return (option == 1 || option == 2 || option == 3 || option == 4 || option == 5 || option == 6);
+    }
+
+    function isVoted() view public returns (bool result){
+        address voter = msg.sender;
+        return (votes[voter] != 0);
     }
 }
