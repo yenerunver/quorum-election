44fce5d0820862cf85a765be73c79c4fd3776c52
yener <yenerunver@hotmail.com>
2019-06-13 01:26:06 +0300

    final commit

--- newCandidate.js
+++ newCandidate.js
@@ -1,20 +1,25 @@
 if(!process.argv[4].length)
-	return "You have to enter a candidate name!";
+	console.log("You have to enter a candidate name!");
 
 if(!process.argv[5].length)
-	return "You have to enter a candidate value!";
+	console.log(return "You have to enter a candidate value!");
 
 var Election = artifacts.require("TurkishElections");
 
 module.exports = function(done) {
-	Election.deployed().then(function(instance) {
-		return instance.newCandidate(process.argv[4], process.argv[5]);
-	}).then(function(result){
-		console.log("New candidate is created: " + process.argv[4] + "(" + process.argv[5] + ")");
-		console.log(result);
+	if(process.argv[4].length && process.argv[5].length){
+		Election.deployed().then(function(instance) {
+			return instance.newCandidate(process.argv[4], process.argv[5]);
+		}).then(function(result){
+			console.log("New candidate is created: " + process.argv[4] + "(" + process.argv[5] + ")");
+			console.log(result);
+			done();
+		}).catch(function(e) {
+			console.log(e);
+			done();
+		});
+	}
+	
+	else
 		done();
-	}).catch(function(e) {
-		console.log(e);
-		done();
-	});
 };
