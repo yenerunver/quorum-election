1c5ef2e45f327594127af3151ee5da4ac125e695
yener <yenerunver@hotmail.com>
2019-06-13 01:30:46 +0300

    final commit

--- isVoted.js
+++ isVoted.js
@@ -1,19 +1,24 @@
-if(!process.argv[4].length)
-	return "You have to enter an account address!";
+if(!process.argv[4])
+	console.log("You have to enter an account address!");
 
 var Election = artifacts.require("TurkishElections");
 
 module.exports = function(done) {
-	Election.deployed().then(function(instance) {
-		return instance.isVoted(process.argv[4]);
-	}).then(function(result){
-		if(result)
-			console.log(process.argv[4] + " has voted.");
-		else
-			console.log(process.argv[4] + " has not voted yet.");
+	if(process.argv[4]){
+		Election.deployed().then(function(instance) {
+			return instance.isVoted(process.argv[4]);
+		}).then(function(result){
+			if(result)
+				console.log(process.argv[4] + " has voted.");
+			else
+				console.log(process.argv[4] + " has not voted yet.");
+			done();
+		}).catch(function(e) {
+			console.log(e);
+			done();
+		});
+	}
+	
+	else
 		done();
-	}).catch(function(e) {
-		console.log(e);
-		done();
-	});
 };
